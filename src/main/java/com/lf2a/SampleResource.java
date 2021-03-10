6c6bbf6878c982dfd043ac0ce9f98ac883d38176
Luiz Filipy <luizfilipy.aa@outlook.com>
2021-03-10 19:20:53 -0300

    Primeiro commit - exemplo de uma validação customizada e listando no README todos os tipos de valirdações

---
+++ src/main/java/com/lf2a/SampleResource.java
@@ -0,0 +1,28 @@
+package com.lf2a;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.lf2a.validation.USPhoneNumber;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("")
+public class SampleResource {
+
+	@Inject
+	@ConfigProperty(name = "message")
+	private String message;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response message(@USPhoneNumber @QueryParam("n") String number) {
+		// %2B1-415-555-2671
+		return Response.ok(message).build();
+	}
+
+}
